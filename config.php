4ddb2ed1b681f6edf434e356b16a2085b97f50ff
ip20.m.pauls <ip20.m.pauls@pjvt.local>
2024-05-29 09:58:36 +0300

    tabula

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "carrental";
+$username = "kvdlv_martins";
+$password = "qoXtus-sudzox-7vemmy";
+$dbname = "kvdlv_carrental";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
