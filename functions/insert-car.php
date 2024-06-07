e83afd2451099d42cc6d67ef366fa0639e4488a5
Mārtiņš . <ip20.m.pauls@vtdt.edu.lv>
2024-06-07 13:38:39 +0300

    fix

--- functions/insert-car.php
+++ functions/insert-car.php
@@ -12,7 +12,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $carName = $_POST['carSearch'] ?? '';
     $transmission = $_POST['transmission'] ?? '';
     $year = $_POST['year'] ?? '';
-    $color = $_POST['color'] ?? '';
     $mileage = $_POST['mileage'] ?? 0;
     $rentalRate = $_POST['rentalRate'] ?? 0.0;
     $description = $_POST['description'] ?? '';
@@ -33,10 +32,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    $sql = "INSERT INTO cars (user_id, car_name, transmission, year, mileage, availability_status, rental_rate, description, image_1, image_2, image_3, image_4, image_5, image_6) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO cars (user_id, car_name, transmission, year, mileage, availability_status, rental_rate, description, image_1, image_2, image_3, image_4, image_5, image_6, image_7, image_8) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
     $availabilityStatus = 1;
-    $stmt->bind_param("ississdssssssss", $user_id, $carName, $transmission, $year, $mileage, $availabilityStatus, $rentalRate, $description, $images[1], $images[2], $images[3], $images[4], $images[5], $images[6]);
+    $stmt->bind_param("ississdssssssss", $user_id, $carName, $transmission, $year, $mileage, $availabilityStatus, $rentalRate, $description, $images[1], $images[2], $images[3], $images[4], $images[5], $images[6], $image[7], $image[8]);
 
     if ($stmt->execute()) {
         header("Location: index.php?success=1");
